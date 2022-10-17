9b2df75872e877e3634d0e98b709fcc13e1c7295
abrar-byte <usamahhafidz99@gmail.com>
2022-10-17 14:05:43 +0700

    first commit

---
+++ src/app/utils/cekInputValid.js
@@ -0,0 +1,4 @@
+const cekInputValid = (...angka) => {
+  return angka.every((num) => typeof num === 'number' && !isNaN(num));
+};
+module.exports = cekInputValid;
---
+++ src/app/utils/parseInput.js
@@ -0,0 +1,4 @@
+const parseInput = (...input) => {
+  return input.map((str) => parseInt(str));
+};
+module.exports = parseInput;
